46634b14b67deb6dc4b1a92f50f57c54cbb5af69
am0392 <andymorgan@hotmail.nl>
2026-04-14 22:40:58 +0200

    Begin of simple loop for demo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to this amazing program");
-Menu.Start();
+﻿class Program
+{
+    static void Main()
+    {
+        bool running = true;
+
+        while (running)
+        {
+            string choice = WelcomeScreen.Menu();
+
+            Console.Clear();
+
+            if (choice == "Login")
+            {
+                UserLogin.Start();
+            }
+            else if (choice == "Continue as Guest")
+            {
+                Menu.Start("Guest");
+            }
+
+            Console.WriteLine("\nPress ESC to quit or any other key to return...");
+            var key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.Escape)
+                running = false;
+        }
+    }
+}
