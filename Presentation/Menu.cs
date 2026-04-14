f60dbc30d97b37725a026fefaab5807c4fe82cd5
am0392 <andymorgan@hotmail.nl>
2026-04-14 22:40:26 +0200

    Update Menu.cs

--- Presentation/Menu.cs
+++ Presentation/Menu.cs
@@ -4,25 +4,26 @@ static class Menu
     //This shows the menu. You can call back to this method to show the menu again
     //after another presentation method is completed.
     //You could edit this to show different menus depending on the user's role
-    static public void Start()
+    static public void Start(string userName)
     {
-        Console.WriteLine("Enter 1 to login");
-        Console.WriteLine("Enter 2 to do something else in the future");
+        Console.Clear();
+        Console.WriteLine($"You are logged in as: {userName}");
+        Console.WriteLine("PLACEHOLDER");
 
-        string input = Console.ReadLine();
-        if (input == "1")
-        {
-            UserLogin.Start();
-        }
-        else if (input == "2")
-        {
-            Console.WriteLine("This feature is not yet implemented");
-        }
-        else
-        {
-            Console.WriteLine("Invalid input");
-            Start();
-        }
+        // string input = Console.ReadLine()!;
+        // if (input == "1")
+        // {
+        //     UserLogin.Start();
+        // }
+        // else if (input == "2")
+        // {
+        //     Console.WriteLine("This feature is not yet implemented");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Invalid input");
+        //     Start(userName);
+        // }
 
     }
 }
