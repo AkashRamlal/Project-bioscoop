b5158ef2e3ac962b2bf0875eb1d6ac0968d2daae
thanya0092-bit <thanya0092@gmail.com>
2026-04-14 21:56:08 +0200

    payment 1.0

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to this amazing program");
-Menu.Start();
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Dictionary<string, Dictionary<string, decimal>> hallData = new Dictionary<string, Dictionary<string, decimal>> {
+            { "Hall 1", new Dictionary<string, decimal> { { "A3", 10.00m }, { "B5", 12.50m } } }
+        };
+        Console.WriteLine("Welcome to this amazing program");
+        Menu.Start();
+        PaymentUI.Start("filmName", "00:00", hallData);
+    }
+}
