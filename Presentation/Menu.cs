64d2db562f1919af1e1ca6419dc8b85a9fb8d9a3
am0392 <andymorgan@hotmail.nl>
2026-04-15 18:09:26 +0200

    Refactor menu and userLogin

--- Presentation/Menu.cs
+++ Presentation/Menu.cs
@@ -1,29 +1,140 @@
-static class Menu
+public static class Menu
 {
+    public static void Start(string role)
+{
+    Auditorium hall1 = new Auditorium();
 
-    //This shows the menu. You can call back to this method to show the menu again
-    //after another presentation method is completed.
-    //You could edit this to show different menus depending on the user's role
-    static public void Start(string userName)
+    Dictionary<string, Dictionary<string, decimal>> hallData = new Dictionary<string, Dictionary<string, decimal>> {
+        { "Hall 1", new Dictionary<string, decimal> { { "A3", 10.00m }, { "B5", 12.50m } } }
+    };
+
+    bool inMenu = true;
+
+    while (inMenu)
+    {
+        string choice = ShowMenu(role);
+
+        Console.Clear();
+
+        switch (choice)
+        {
+            case "Movie theatre info":
+                Console.WriteLine("pizza");
+                break;
+
+            case "View movies":
+                hall1.StartSelection();
+                PaymentUI.Start("filmName", "00:00", hallData);
+                break;
+
+            case "Your tickets":
+                Console.WriteLine("pizza");
+                break;
+
+            case "Manage films":
+                Console.WriteLine("pizza");
+                break;
+
+            case "Manage tickets":
+                Console.WriteLine("pizza");
+                break;
+
+            case "Manage employees":
+                Console.WriteLine("pizza");
+                break;
+
+            case "Quit":
+                inMenu = false;
+                continue;
+        }
+
+        Console.WriteLine("\nPress any key to return to menu...");
+        Console.ReadKey();
+    }
+}
+    public static string ShowMenu(string role)
+    {
+        List<string> menuOptions = GetOptions(role);
+
+        int selectedIndex = 0;
+        ConsoleKey key;
+
+        do
+        {
+            DrawMenu(menuOptions, selectedIndex, role);
+
+            key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.UpArrow)
+            {
+                selectedIndex--;
+                if (selectedIndex < 0)
+                    selectedIndex = menuOptions.Count - 1;
+            }
+            else if (key == ConsoleKey.DownArrow)
+            {
+                selectedIndex++;
+                if (selectedIndex >= menuOptions.Count)
+                    selectedIndex = 0;
+            }
+
+        } while (key != ConsoleKey.Enter);
+
+        return menuOptions[selectedIndex];
+    }
+
+    private static List<string> GetOptions(string role)
+    {
+        List<string> options = [];
+
+        // Member
+        options.Add("Movie theatre info");
+        options.Add("View movies");
+        options.Add("Your tickets");
+        
+
+        // Employee
+        if (role == "employee" || role == "admin")
+        {
+            options.Add("Manage films");
+            options.Add("Manage tickets");
+        }
+
+        // Admin
+        if (role == "admin")
+        {
+            options.Add("Manage employees");
+        }
+
+        options.Add("Quit");
+
+        return options;
+    }
+
+    private static void DrawMenu(List<string> options, int selectedIndex, string role)
     {
         Console.Clear();
-        Console.WriteLine($"You are logged in as: {userName}");
-        Console.WriteLine("PLACEHOLDER");
 
-        // string input = Console.ReadLine()!;
-        // if (input == "1")
-        // {
-        //     UserLogin.Start();
-        // }
-        // else if (input == "2")
-        // {
-        //     Console.WriteLine("This feature is not yet implemented");
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Invalid input");
-        //     Start(userName);
-        // }
+        Console.WriteLine("=====================================");
+        Console.WriteLine("         Movie theatre Menu");
+        Console.WriteLine("=====================================\n");
 
+        Console.WriteLine($"You are logged in as: {role}");
+
+        Console.WriteLine("Use arrow keys to navigate and press Enter to select option:\n");
+
+        for (int i = 0; i < options.Count; i++)
+        {
+            if (i == selectedIndex)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"> {options[i]}");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine($"  {options[i]}");
+            }
+        }
     }
 }
--- Presentation/UserLogin.cs
+++ Presentation/UserLogin.cs
@@ -3,7 +3,7 @@ static class UserLogin
     static private AccountsLogic accountsLogic = new AccountsLogic();
 
 
-    public static void Start()
+    public static AccountModel? Start()
     {
         Console.Clear();
         Console.WriteLine("Welcome to the login page\n");
@@ -14,12 +14,12 @@ static class UserLogin
         AccountModel acc = accountsLogic.CheckLogin(email, password);
         if (acc != null)
         {
-            //Write some code to go back to the menu
-            Menu.Start(acc.FullName);
+            return acc;
         }
         else
         {
             Console.WriteLine("No account found with that email and password");
+            return null;
         }
     }
 }
