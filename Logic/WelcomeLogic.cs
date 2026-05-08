f01fd1e1218798250ead726496f7f74b10d488c0
am0392 <andymorgan@hotmail.nl>
2026-05-08 15:40:06 +0200

    Added list length check to prevent exception

--- Logic/WelcomeLogic.cs
+++ Logic/WelcomeLogic.cs
@@ -5,9 +5,18 @@ public static class WelcomeLogic
         Random rand = new();
         List<string> returnList = [];
 
+        if (films.Count < 5)
+        {
+            foreach (var film in films)
+            {
+                returnList.Add(film.Naam);
+            }
+            return returnList;
+        }
+
         for (int i = 5; i <= 0; i--)
         {
-            string addFilm = films[i].Naam;
+            string addFilm = films[i].Naam!;
             returnList.Add(addFilm);
         }
 
