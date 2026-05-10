72f23ad72d32e46621c7f45b39407f92c9ee8c1f
am0392 <andymorgan@hotmail.nl>
2026-05-10 21:00:48 +0200

    Added UnitTest folder

--- Presentation/TheatreInfo.cs
+++
@@ -1,9 +0,0 @@
-public static class TheatreInfo
-{
-    public static void Print()
-    {
-        Console.WriteLine();
-        Console.WriteLine("Theatre adress:");
-        Console.WriteLine("Wijnhaven 107, 3011 WN in Rotterdam");
-    }
-}
